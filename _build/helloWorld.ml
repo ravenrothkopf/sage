214b1cbd21a2f9498f980d3326cd745abdea03eb
Raven Rothkopf <rgr2124@barnard.edu>
2022-11-20 17:23:42 -0500

    removing build, oops!

--- _build/helloWorld.ml
+++
@@ -1,9 +0,0 @@
-str hello_str = " hi "
-
-str funct main (str h):
-    str location = " world "
-    h = " hello"
-    str ran = " "
-    print (h + " " + location)
-
-// make sure you don't copy the indents, and actually use the tab button!
