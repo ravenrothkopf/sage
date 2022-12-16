e59e8b31a29b4d9a152332090e5ffc88f303cc51
Lauren <lmc2265@columbia.edu>
2022-12-16 11:18:11 -0500

    test shell script

--- tests/testall.sh
+++ tests/testall.sh
@@ -80,8 +80,8 @@ RunFail() {
 Check() {
     error=0
     basename=`echo $1 | sed 's/.*\\///
-                             s/.mc//'`
-    reffile=`echo $1 | sed 's/.mc$//'`
+                             s/.sage//'`
+    reffile=`echo $1 | sed 's/.sage$//'`
     basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
 
     echo -n "$basename..."
@@ -113,8 +113,8 @@ Check() {
 CheckFail() {
     error=0
     basename=`echo $1 | sed 's/.*\\///
-                             s/.mc//'`
-    reffile=`echo $1 | sed 's/.mc$//'`
+                             s/.sage//'`
+    reffile=`echo $1 | sed 's/.sage$//'`
     basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
 
     echo -n "$basename..."
@@ -168,7 +168,7 @@ if [ $# -ge 1 ]
 then
     files=$@
 else
-    files="tests/test-*.mc tests/fail-*.mc"
+    files="tests/test-*.sage tests/fail-*.sage"
 fi
 
 for file in $files
