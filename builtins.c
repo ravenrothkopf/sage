d4f0ba6fac8f2a8ba261ec45c96edaff8f2d00cf
Raven Rothkopf <rgr2124@barnard.edu>
2022-12-17 14:38:10 -0500

    change concatenation, add binary ops and boolean logic to semantic analysis and code gen

--- builtins.c
+++ builtins.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 char* concat(const char *str1, const char *str2)
 {
     size_t len1 = strlen(str1);
     size_t len2 = strlen(str2);
-    char* both = (char*)malloc(len1 + len2 + 1);
-    strcpy(both, str2);
-    strcat(both, str1);
-    return both;
+    char* newStr = (char*)malloc(len1 + len2 + 1);
+    strcpy(newStr, str1);
+    strcat(newStr, str2);
+    return newStr;
 }
