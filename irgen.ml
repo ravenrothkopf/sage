ce48d45e36e583b8bb22f7923a0dd9b89b5f0a8a
Raven Rothkopf <rgr2124@barnard.edu>
2022-12-19 13:06:28 -0500

    fix makefile, modulus, and if else test case

--- irgen.ml
+++ irgen.ml
@@ -180,7 +180,7 @@ in
          | A.Sub     -> L.build_sub
          | A.Mul     -> L.build_mul
          | A.Div     -> L.build_sdiv
-         | A.Mod     -> L.const_srem
+         | A.Mod     -> L.build_srem
          | A.And     -> L.build_and
          | A.Or      -> L.build_or
          | A.Equal   -> L.build_icmp L.Icmp.Eq
